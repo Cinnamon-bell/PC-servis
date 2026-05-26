6b175e76a2c6b7b15c4abee0efe351c6582d3887
Cinnamon-bell <palinko.lompart@gmail.com>
2026-05-26 10:02:45 +0200

    Pridanie ID pre komponenty, úprava vzhľadu (nedokončené), príprava controllera na prácu

--- src/sk/spse/pcservis/ui/Controller.java
+++ src/sk/spse/pcservis/ui/Controller.java
@@ -1,46 +1,16 @@
 package sk.spse.pcservis.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 
 /**
- * Controller pre FXML súbor – obsahuje logiku aplikácie
+ * Controller pre FXML súbor – obsahuje logiku aplikácie ||getKomponentyPodlaKategorie
  */
 public class Controller {
 
     @FXML
-    private TextArea textArea;
+    private TableView<inventar_table> inventar_table;
 
-    @FXML
-    private void ruka() {
-        textArea.appendText("ruka pridana\n");
-        textArea.setScrollTop(Double.MAX_VALUE);
-    }
 
-    @FXML
-    private void noha() {
-        textArea.appendText("noha pridana\n");
-        textArea.setScrollTop(Double.MAX_VALUE);
-    }
-
-    @FXML
-    private void hlava() {
-        textArea.appendText("hlava pridana\n");
-        textArea.setScrollTop(Double.MAX_VALUE);
-    }
-
-    @FXML
-    private void trup() {
-        textArea.appendText("trup pridana\n");
-        textArea.setScrollTop(Double.MAX_VALUE);
-    }
-
-    @FXML
-    private void poskladaj() {
-        textArea.appendText("Montujem!!!\n");
-        textArea.appendText("____________\n");
-        textArea.appendText("ROBOT HOTOVÝ\n");
-        textArea.appendText("____________\n");
-        textArea.setScrollTop(Double.MAX_VALUE);
-    }
 }
